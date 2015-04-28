454a64f62f859bc26d20247851fdcfdea7b3e759
kanyapandey <kanyapandey2603@gmail.com>
2015-04-28 16:17:16 +0700

    number14

--- main.c
+++ main.c
@@ -3,10 +3,9 @@
 
 int main()
 {
-<<<<<<< HEAD:main.c
-    printf("wow!\n");
-=======
+
     printf("yeah!\n");
->>>>>>> 8dc1d1ec89a6f2af0640752d4e3e08e6391483e5:main.c.c
+    printf("wow!!\n")
+
     return 0;
 }
